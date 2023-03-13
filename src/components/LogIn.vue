d697645128f12b835c4e2302e1c92a91cd690107
papiricoh <slendeman0200@gmail.com>
2023-03-13 12:15:51 +0100

    added format to log-in

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -3,6 +3,8 @@
         <div class="box flex_column">
             <h2 class="title">Log-In</h2>
             <p>Email And Password required</p>
+            <input class="input_bar" type="email" placeholder="ecomerce@gmail.com">
+            <input class="input_bar" type="password" placeholder="********">
         </div>
     </div>
 </template>
