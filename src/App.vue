b80c4cfce7f5a96ffd5a1001d69181290581d960
papiricoh <slendeman0200@gmail.com>
2023-03-13 09:36:23 +0100

    First Steps of webUi

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,45 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div id="main_header">
+      <ul class="horizontal_menu">
+        <li><RouterLink class="text_deco_none" to="/">Home</RouterLink></li>
+        <li><RouterLink class="text_deco_none" to="/about">About</RouterLink></li>
+      </ul>
+      <img height="140" src="https://dieselpunkcore.com/wp-content/uploads/2014/06/logo-placeholder.png" alt="logo">
+      <ul class="horizontal_menu">
+        <li><RouterLink class="text_deco_none" to="/">Log-In</RouterLink></li>
+        <li><RouterLink class="text_deco_none" to="/about">Register</RouterLink></li>
+      </ul>
     </div>
   </header>
-
-  <RouterView />
+  <div class="info_body">
+    <div class="product_menu">
+      <ul>
+        <div class="list_category"> 
+          <li class="bald_menu_item">MEN</li>
+          <ul class="sub_category_list">
+            <li>SUITS</li>
+            <li>OUTERWEAR</li>
+            <li>SHIRTS</li>
+            <li>BLAZERS</li>
+            <li>POLOS & T-SHIRTS</li>
+            <li>PANTS</li>
+          </ul>
+        </div>
+        <div class="list_category"> 
+          <li class="bald_menu_item">WOMEN</li>
+          <ul class="sub_category_list">
+            <li>SUITS</li>
+            <li>OUTERWEAR</li>
+            <li>SHIRTS</li>
+            <li>BLAZERS</li>
+            <li>POLOS & T-SHIRTS</li>
+            <li>PANTS</li>
+          </ul>
+        </div>
+      </ul>
+    </div>
+    <RouterView />
+  </div>
 </template>
