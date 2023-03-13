09f5d030d1d3a6a20223d1dbfd98add6426f1985
papiricoh <slendeman0200@gmail.com>
2023-03-13 13:35:03 +0100

    Added show and hide menu

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,32 @@ import { RouterLink, RouterView } from 'vue-router'
 import ProductMenu from './components/ProductMenu.vue';
 </script>
 
+<script>
+export default {
+  data() {
+    return {
+      product_menu: true
+    }
+  },
+}
+</script>
+
 <template>
   <header>
     <div id="main_header">
       <ul class="horizontal_menu">
-        <li><RouterLink class="text_deco_none" to="/">Home</RouterLink></li>
-        <li><RouterLink class="text_deco_none" to="/about">About</RouterLink></li>
+        <li><RouterLink @click="product_menu = true" class="text_deco_none" to="/">Home</RouterLink></li>
+        <li><RouterLink @click="product_menu = true" class="text_deco_none" to="/about">About</RouterLink></li>
       </ul>
       <img height="140" src="https://dieselpunkcore.com/wp-content/uploads/2014/06/logo-placeholder.png" alt="logo">
       <ul class="horizontal_menu">
-        <li><RouterLink class="text_deco_none" to="/log-in">Log-In</RouterLink></li>
-        <li><RouterLink class="text_deco_none" to="/register">Register</RouterLink></li>
+        <li><RouterLink @click="product_menu = false" class="text_deco_none" to="/log-in">Log-In</RouterLink></li>
+        <li><RouterLink @click="product_menu = false" class="text_deco_none" to="/register">Register</RouterLink></li>
       </ul>
     </div>
   </header>
   <div class="info_body">
-    <div class="product_menu">
+    <div v-if="product_menu" class="product_menu">
       <ProductMenu />
     </div>
     <RouterView />
