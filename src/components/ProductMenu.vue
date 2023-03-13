b282f401f27438bf8ed9b4414f4780467e1b9c3b
papiricoh <slendeman0200@gmail.com>
2023-03-13 09:48:25 +0100

    Formated list

--- src/components/ProductMenu.vue
+++ src/components/ProductMenu.vue
@@ -2,26 +2,26 @@
     <div>
         <ul>
             <div class="list_category"> 
-            <li class="bald_menu_item">MEN</li>
-            <ul class="sub_category_list">
-                <li>SUITS</li>
-                <li>OUTERWEAR</li>
-                <li>SHIRTS</li>
-                <li>BLAZERS</li>
-                <li>POLOS & T-SHIRTS</li>
-                <li>PANTS</li>
-            </ul>
+                <li class="bald_menu_item">MEN</li>
+                <ul class="sub_category_list">
+                    <li>SUITS</li>
+                    <li>OUTERWEAR</li>
+                    <li>SHIRTS</li>
+                    <li>BLAZERS</li>
+                    <li>POLOS & T-SHIRTS</li>
+                    <li>PANTS</li>
+                </ul>
             </div>
             <div class="list_category"> 
-            <li class="bald_menu_item">WOMEN</li>
-            <ul class="sub_category_list">
-                <li>SUITS</li>
-                <li>OUTERWEAR</li>
-                <li>SHIRTS</li>
-                <li>BLAZERS</li>
-                <li>POLOS & T-SHIRTS</li>
-                <li>PANTS</li>
-            </ul>
+                <li class="bald_menu_item">WOMEN</li>
+                <ul class="sub_category_list">
+                    <li>SUITS</li>
+                    <li>OUTERWEAR</li>
+                    <li>SHIRTS</li>
+                    <li>BLAZERS</li>
+                    <li>POLOS & T-SHIRTS</li>
+                    <li>PANTS</li>
+                </ul>
             </div>
         </ul>
     </div>
