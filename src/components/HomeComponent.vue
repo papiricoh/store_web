0384e39c15b84adef6b2d2e5e2a3b26e637f5e7e
papiricoh <slendeman0200@gmail.com>
2023-03-16 21:18:31 +0100

    added content card to style

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -16,7 +16,7 @@ export default {
   <div class="flex_column">
     <div class="hero">
       <div id="main_title">Scylla</div>
-      <div class="title">Hypebeast</div>
+      <div class="title" id="main_subtitle">Hypebeast</div>
     </div>
     <div class="section">
       <div>
